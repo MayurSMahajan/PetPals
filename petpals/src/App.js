e9888cd0a4fb0b0ed903b85a314a43482e8f6167
MayurSMahajan <mayur23smahajan@gmail.com>
2022-08-06 22:04:56 +0530

    Add Pet Sitter Screen

--- petpals/src/App.js
+++ petpals/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import SelectPet from "./components/SelectPet";
+import BecomeSitter from "./components/BecomeSitter";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <SelectPet />
+      <BecomeSitter/>
     </div>
   );
 }
---
+++ petpals/src/components/BecomeSitter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PendingRequests from './PendingRequests';
+import UserNav from './UserNav';
+
+function BecomeSitter() {
+  return (
+    <div className="">
+        <UserNav/>
+        <PendingRequests/>
+    </div>
+  );
+}
+
+export default BecomeSitter;
