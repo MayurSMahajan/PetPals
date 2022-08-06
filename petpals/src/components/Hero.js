bdaf8aba601608214cf9f189a2f1f3e2dee2c21e
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2022-08-06 16:43:41 +0530

    hero component created

--- petpals/src/components/Hero.js
+++ petpals/src/components/Hero.js
@@ -1,17 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import heroIllustration from "./heroIllustration.svg";
 const Hero = () => {
-    return (
-        <div className='flex m-2 min-h-[85vh] w-full'>
-            <div className='flex flex-col w-2/3'>
-                <h1 className='text-xl'>Welcome to PetPals</h1>
-            </div>
-            <div className='flex flex-col w-1/3'>
-                <button className='rounded-md p-2 w-1/2 items-center m-1 hover:bg-blue-900 bg-blue-500 text-white'>Continue as a Pet Owner</button>
-                <button className='rounded-md p-2 w-1/2 items-center m-1 hover:bg-blue-900 bg-blue-500 text-white'>Continue as a Pet Sitter</button>
-            </div>            
+  return (
+    <div classname="hero-container">
+      <div className="flex flex-row justify-around mt-20">
+        <div className="hero-headline flex flex-col pl-10">
+          <h1 className="text-5xl font-bold  ">
+            Find the best pet sitter for your furry friend!
+          </h1>
+          <p className="w-62 mt-4 text-lg">
+            Are you looking for someone who can take good care of your pet?
+            <br />
+            We have you covered, search for verified Pet Sitters around your
+            area now.
+          </p>
+          <div className="flex mt-28 ">
+            <button className="bg-apnacolor hover:bg-apnacolor text-white font-bold py-2 px-4  rounded">
+              Find a Sitter
+            </button>
+            <button className="ml-3 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-apnacolor rounded shadow">
+              Become a Sitter
+            </button>
+          </div>
         </div>
-    )
-}
+        <div className="hero-illustration flex-2">
+          <img src={heroIllustration} alt="hero" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
